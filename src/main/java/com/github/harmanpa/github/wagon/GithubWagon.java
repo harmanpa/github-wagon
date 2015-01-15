14413741085a7528b9b4a4eb875e44c0d7ef5257
Peter Harman <peter.harman@deltatheta.com>
2015-01-15 12:49:29 +0000

    Changed to egit github API as github-api did not support blobs

--- src/main/java/com/github/harmanpa/github/wagon/GithubWagon.java
+++ src/main/java/com/github/harmanpa/github/wagon/GithubWagon.java
@@ -1,6 +1,8 @@
-
 package com.github.harmanpa.github.wagon;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
+import com.google.common.io.Files;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
@@ -11,10 +13,14 @@ import org.apache.maven.wagon.ResourceDoesNotExistException;
 import org.apache.maven.wagon.TransferFailedException;
 import org.apache.maven.wagon.authentication.AuthenticationException;
 import org.apache.maven.wagon.authorization.AuthorizationException;
-import org.codehaus.plexus.util.FileUtils;
-import org.kohsuke.github.GHContent;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
+import org.codehaus.plexus.util.Base64;
+import org.eclipse.egit.github.core.Blob;
+import org.eclipse.egit.github.core.IRepositoryIdProvider;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.TreeEntry;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.DataService;
+import org.eclipse.egit.github.core.service.RepositoryService;
 
 /**
  *
@@ -57,21 +63,26 @@ public class GithubWagon extends AbstractWagon {
                     }
                 }
             }
-            // Connect to GitHub API
-            GitHub github = GitHub.connectUsingPassword(getAuthenticationInfo().getUserName(), getAuthenticationInfo().getPassword());
-            if (repo == null) {
-                throw new ResourceDoesNotExistException("No repository specified");
+            GitHubClient client = new GitHubClient();
+            client.setCredentials(getAuthenticationInfo().getUserName(), getAuthenticationInfo().getPassword());
+            RepositoryService repositoryService = new RepositoryService(client);
+            Repository gitRepo = repositoryService.getRepository(owner, repo);
+            DataService dataService = new DataService(client);
+            Blob blob = findBlob(dataService, gitRepo, string, branch);
+            if (blob == null) {
+                throw new ResourceDoesNotExistException("Could not find " + string);
             }
-            GHRepository ghRepo = github.getRepository(owner + "/" + repo);
-            if (ghRepo == null) {
-                throw new ResourceDoesNotExistException("Not connected to repository");
+            byte[] data;
+            if ("base64".equals(blob.getEncoding())) {
+                data = Base64.decodeBase64(blob.getContent().getBytes());
+            } else {
+                data = blob.getContent().getBytes();
             }
-            GHContent content = branch == null ? ghRepo.getFileContent(string) : ghRepo.getFileContent(string, branch);
-            FileUtils.fileWrite(file, content.getContent());
+            Files.write(data, file);
         } catch (URISyntaxException ex) {
-            throw new TransferFailedException("Failed to connect to URL", ex);
+            throw new ResourceDoesNotExistException("Could not parse repository URL");
         } catch (IOException ex) {
-            throw new TransferFailedException("Failed to transfer " + string, ex);
+            throw new TransferFailedException("Error during transfer", ex);
         }
     }
 
@@ -85,4 +96,22 @@ public class GithubWagon extends AbstractWagon {
     public void put(File file, String string) throws TransferFailedException, ResourceDoesNotExistException, AuthorizationException {
     }
 
+    private Blob findBlob(DataService service, IRepositoryIdProvider repo, String path, String branch) throws IOException {
+        String branchSha = service.getReference(repo, "heads/" + (branch == null ? "master" : branch)).getObject().getSha();
+        Iterable<String> pathSplit = Splitter.on('/').omitEmptyStrings().split(path);
+        return findBlob(service, repo, pathSplit, branchSha, 0);
+    }
+
+    private Blob findBlob(DataService service, IRepositoryIdProvider repo, Iterable<String> path, String sha, int depth) throws IOException {
+        if (depth == Iterables.size(path)) {
+            return service.getBlob(repo, sha);
+        }
+        String name = Iterables.get(path, depth);
+        for (TreeEntry entry : service.getTree(repo, sha).getTree()) {
+            if (name.equals(entry.getPath())) {
+                return findBlob(service, repo, path, entry.getSha(), depth + 1);
+            }
+        }
+        return null;
+    }
 }
